4f24941ffd195329d3cc20eb2e87b843c9b3e2b8
bsrodriguezr <bruiz040@gmail.com>
2020-06-14 18:39:05 -0500

    Se actualiza hasta clase 7

---
+++ Clase7/ladingPage/js/index.js
@@ -0,0 +1,23 @@
+function switchTheme() {
+    if (document.theme === 'light') {
+        replaceClasses('light', 'dark');
+    } else {
+        replaceClasses('dark', 'light');
+    }
+}
+
+function replaceClasses(oldClass, newClass) {
+    document.theme = newClass;
+    var elements = document.getElementsByClassName(oldClass);
+    while (elements.length > 0) {
+        elements[0].classList.replace(oldClass, newClass);
+    }
+    var button = document.getElementById('theme');
+    button.innerHTML = newClass.toUpperCase();
+}
+
+var btnTheme=document.getElementById('theme')
+
+btnTheme.addEventListener('click',function(){
+    switchTheme()
+})
